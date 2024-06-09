6c2f2b0a1c5ef9b4c29f2725aa9e516f253e40b0
jpoechill <poamrong@rith.com>
2024-06-08 19:45:38 -0700

    updates to copy

--- app/Header.tsx
+++ app/Header.tsx
@@ -41,7 +41,7 @@ export default function Header() {
                 <Link href="/testimonials">Testimonials</Link>
               </div>
               <div>
-                <Link href="/contact">Contact</Link>
+                <Link href="/contact/electronic">Contact</Link>
               </div>
             </div>
             <div className="hidden lg:flex flex-row lg:items-center gap-5 text-lg">
--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -10,9 +10,6 @@ export default function Home() {
       <br /><br />
       Our hours are 9am-5pm, Monday through Friday.
       <br /><br />
-      <h1>Locations</h1><br />
-      Quality and craftsmanship are the cornerstones of our work. We are committed to providing the highest quality service, ensuring that every restoration project not only meets but exceeds client expectations. Our dedication to excellence guarantees that the beauty and integrity of your building are preserved for generations to come.
-      <br /><br />
     </main>
   );
 }
